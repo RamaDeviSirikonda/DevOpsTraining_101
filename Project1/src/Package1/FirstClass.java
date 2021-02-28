fe9d9e0e7fb49a35bf518067f89f753dd03cf3fd
Ramadevi <Ramadevi@LAPTOP-GJ75CQOS>
2021-02-28 21:16:39 +0530

    updated my class file with new row addition. 2nd commit

--- Project1/src/Package1/FirstClass.java
+++ Project1/src/Package1/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, this is my first program in Dev Ops training.");
+		System.out.println("Hurray, my first day assignment completed successfully..");
 	}
 
 }
